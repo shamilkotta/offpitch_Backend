51fc0a2223ea1f3d2180b004df2facfc11fc96d8
shamilkotta <shamilkotta99@gmail.com>
2023-02-04 10:15:20 +0530

    feat: errorhadnling update

--- src/utils/ErrorResponse.js
+++
@@ -1,9 +0,0 @@
-class ErrorResponse extends Error {
-  constructor(status, message) {
-    super(message);
-    this.status = status;
-    this.message = message;
-  }
-}
-
-export default ErrorResponse;
