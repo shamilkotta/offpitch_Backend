b2cfbfca4968b72cda6ca98026516c91a2625a1a
shamilkotta <shamilkotta99@gmail.com>
2023-02-04 10:22:26 +0530

    feat: signup validation

---
+++ src/middlewares/validations/signupValidation.js
@@ -0,0 +1,48 @@
+import * as yup from "yup";
+import ErrorResponse from "../../error/ErrorResponse.js";
+
+const signupSchema = yup.object().shape({
+  name: yup
+    .string()
+    .transform((value) =>
+      value !== null ? value.charAt(0).toUpperCase() + value.slice(1) : value
+    )
+    .trim()
+    .required("Name can not be empty")
+    .test("isPerfectString", "Enter a valid name", (arg) =>
+      /^[A-Za-z ]+$/.test(arg)
+    ),
+  email: yup
+    .string()
+    .trim()
+    .required("Email can not be empty")
+    .email("Enter a valid email"),
+  password: yup
+    .string()
+    .trim()
+    .required("Password can not be empty")
+    .min(8, "Too short password")
+    .max(16, "Too long password")
+    .test("isPerfectPasswrod", "Enter a strong password", (arg) =>
+      /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])(?!.*\s).{8,16})/.test(arg)
+    ),
+});
+
+const signupValidation = (req, res, next) => {
+  const { name, email, password } = req.body;
+  signupSchema
+    .validate(
+      { name, email, password },
+      { stripUnknown: true, abortEarly: false }
+    )
+    .then((data) => {
+      req.validData = data;
+      next();
+    })
+    .catch((err) => {
+      const [validationErr] = err.errors;
+      next(ErrorResponse.badRequest(validationErr));
+    });
+};
+
+export default signupValidation;
