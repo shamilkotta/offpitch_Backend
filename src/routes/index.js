2b260cd881eaafb1f24b712b8ea0d47aca4e6adb
shamilkotta <shamilkotta99@gmail.com>
2023-02-04 10:18:46 +0530

    signup routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 
+import signupController from "../controllers/index.js";
+import signupValidation from "../middlewares/validations/signupValidation.js";
+
 const router = express.Router();
 
-router.get("/");
+router.post("/signup", signupValidation, signupController);
 
 export default router;
