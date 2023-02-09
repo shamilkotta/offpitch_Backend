fa7afa2ccd3dce36fc2a4c27df36afd76b72a6d3
shamilkotta <shamilkotta99@gmail.com>
2023-02-10 02:11:51 +0530

    feat: create club db schema

---
+++ src/models/club.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const clubSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    profile: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    players: {
+      type: [
+        new mongoose.Schema({
+          name: {
+            type: String,
+            required: true,
+          },
+          date_of_birth: {
+            type: Date,
+            required: true,
+          },
+          profile: {
+            type: String,
+            required: true,
+          },
+          status: {
+            type: String,
+            required: true,
+            default: "active",
+          },
+        }),
+      ],
+    },
+    followers: {
+      type: [mongoose.Schema.Types.ObjectId],
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "active",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Club", clubSchema);
