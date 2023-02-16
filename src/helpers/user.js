ab47b839188c7a52e8d75ed78ed776a4f2af82f0
shamilkotta <shamilkotta99@gmail.com>
2023-02-16 07:14:21 +0530

    feat: create helper for get org and club data

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -1,59 +1,91 @@
 import Club from "../models/club.js";
+import Organization from "../models/organization.js";
 
 export const getClubData = (filter) =>
   new Promise((resolve, reject) => {
-    try {
-      Club.aggregate([
-        {
-          $match: filter,
-        },
-        {
-          $addFields: {
-            followers: { $size: "$followers" },
-            players: {
-              $map: {
-                input: "$players",
-                as: "player",
-                in: {
-                  name: "$$player.name",
-                  profile: "$$player.profile",
-                  _id: "$$player._id",
-                  age: {
-                    $floor: {
-                      $divide: [
-                        { $subtract: [new Date(), "$$player.date_of_birth"] },
-                        365.25 * 24 * 60 * 60 * 1000,
-                      ],
-                    },
+    Club.aggregate([
+      {
+        $match: filter,
+      },
+      {
+        $addFields: {
+          followers: { $size: "$followers" },
+          players: {
+            $map: {
+              input: "$players",
+              as: "player",
+              in: {
+                name: "$$player.name",
+                profile: "$$player.profile",
+                _id: "$$player._id",
+                age: {
+                  $floor: {
+                    $divide: [
+                      { $subtract: [new Date(), "$$player.date_of_birth"] },
+                      365.25 * 24 * 60 * 60 * 1000,
+                    ],
                   },
                 },
               },
             },
           },
         },
-        {
-          $addFields: {
-            followers: {
-              $cond: {
-                if: { $gte: ["$followers", 1000] },
-                then: {
-                  $cond: {
-                    if: { $gte: ["$followers", 1000000] },
-                    then: { $concat: ["$followers", "M"] },
-                    else: { $concat: ["$followers", "K"] },
-                  },
+      },
+      {
+        $addFields: {
+          followers: {
+            $cond: {
+              if: { $gte: ["$followers", 1000] },
+              then: {
+                $cond: {
+                  if: { $gte: ["$followers", 1000000] },
+                  then: { $concat: ["$followers", "M"] },
+                  else: { $concat: ["$followers", "K"] },
                 },
-                else: "$followers",
               },
+              else: "$followers",
             },
           },
         },
-      ]).then((res) => {
+      },
+    ])
+      .then((res) => {
         resolve(res[0]);
-      });
-    } catch (err) {
-      reject(err);
-    }
+      })
+      .catch(reject);
   });
 
-export const hi = {};
+export const getOrganizationData = (filter) =>
+  new Promise((resolve, reject) => {
+    Organization.aggregate([
+      {
+        $match: filter,
+      },
+      {
+        $addFields: {
+          followers: { $size: "$followers" },
+        },
+      },
+      {
+        $addFields: {
+          followers: {
+            $cond: {
+              if: { $gte: ["$followers", 1000] },
+              then: {
+                $cond: {
+                  if: { $gte: ["$followers", 1000000] },
+                  then: { $concat: ["$followers", "M"] },
+                  else: { $concat: ["$followers", "K"] },
+                },
+              },
+              else: "$followers",
+            },
+          },
+        },
+      },
+    ])
+      .then((res) => {
+        resolve(res[0]);
+      })
+      .catch(reject);
+  });
