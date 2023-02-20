3a87d3e2f79962700058e7955bbe4f4bd0e68133
shamilkotta <shamilkotta99@gmail.com>
2023-02-20 10:00:07 +0530

    refactor: remove organization routes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -9,33 +9,20 @@ import {
 import {
   getClubController,
   putClubController,
-  putOrganizationController,
   postPlayerController,
-  getOrganizationController,
 } from "../controllers/userController.js";
 import processImage from "../middlewares/processImage.js";
-import organizationValidation from "../middlewares/validations/user/organization.js";
+import clubValidation from "../middlewares/validations/user/club.js";
 import playerValidation from "../middlewares/validations/user/player.js";
 import tournamentValidation from "../middlewares/validations/user/tournament.js";
 
 const router = express.Router();
 
-// create or update organization
-router.put(
-  "/organization",
-  processImage,
-  organizationValidation,
-  putOrganizationController
-);
-
-// get organization data
-router.get("/organization", getOrganizationController);
-
 // get signature for image upload to cloudinary
 router.get("/image-signature", imageSignatureController);
 
 // create or update club
-router.put("/club", processImage, organizationValidation, putClubController);
+router.put("/club", processImage, clubValidation, putClubController);
 
 // get club data
 router.get("/club", getClubController);
