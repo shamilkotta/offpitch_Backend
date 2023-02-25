df9a7093b9e031fdf5eedfef85edd01541480972
shamilkotta <shamilkotta99@gmail.com>
2023-02-25 16:30:08 +0530

    feat: create admin authorization

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 import ErrorResponse from "../error/ErrorResponse.js";
 
-const userAuthorization = async (req, res, next) => {
+export const userAuthorization = async (req, res, next) => {
   const token = req.headers?.authorization?.split(" ")[1];
   if (!token) return next(ErrorResponse.unauthorized("Unauthorized"));
 
@@ -21,4 +21,24 @@ const userAuthorization = async (req, res, next) => {
   return next();
 };
 
-export default userAuthorization;
+export const adminAuthorization = async (req, res, next) => {
+  const token = req.headers?.authorization?.split(" ")[1];
+  if (!token) return next(ErrorResponse.unauthorized("Unauthorized"));
+
+  // verify token
+  let decode;
+  try {
+    decode = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  } catch (err) {
+    return next(ErrorResponse.forbidden("Forbidden"));
+  }
+
+  if (!decode.data.email || !decode.data.id)
+    return next(ErrorResponse.forbidden("Forbidden"));
+
+  if (decode.data.email !== process.env.ADMIN_EMAIL)
+    return next(ErrorResponse.forbidden("Unauthorized"));
+
+  req.userData = decode.data;
+  return next();
+};
