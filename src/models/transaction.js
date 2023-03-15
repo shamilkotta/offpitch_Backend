f8ff1798611bd3a3be9a4c0fc6828f574b3f6f87
shamilkotta <shamilkotta99@gmail.com>
2023-03-16 04:27:22 +0530

    feat: add timestamp to transaction documents

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,33 +1,36 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-  from: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
+const transactionSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    order_id: {
+      type: String,
+      required: true,
+    },
+    payment_id: {
+      type: String,
+    },
+    signature: {
+      type: String,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  to: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  order_id: {
-    type: String,
-    required: true,
-  },
-  payment_id: {
-    type: String,
-  },
-  signature: {
-    type: String,
-  },
-  status: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Transaction", transactionSchema);
