b661012a94438ef25a3c183247eeb1a7b279e9b3
shamilkotta <shamilkotta99@gmail.com>
2023-04-11 13:01:16 +0530

    refactor: update email sender

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -3,12 +3,8 @@ import * as nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    type: "OAuth2",
     user: process.env.G_MAIL_USERNAME,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    accessToken: process.env.OAUTH_ACCESS_TOKEN,
+    pass: process.env.G_MAIL_PASS,
   },
 });
 
