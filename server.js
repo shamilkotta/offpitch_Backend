ba1901b37e7a4098bc6eb66c148d0dabad6dc9ae
SHAMIL <64640025+shamilkotta@users.noreply.github.com>
2023-07-04 07:21:22 +0530

    port config change

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 5000;
 app.set("port", port);
 
 const server = http.createServer(app);
-server.listen(port, "127.0.0.1", () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
