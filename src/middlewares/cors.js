c43cd9d63ffb695adafc450a1441cca4eddcc0b5
shamilkotta <shamilkotta99@gmail.com>
2023-07-05 21:25:33 +0530

    fix: allow all origins

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Authorization, Accept"
