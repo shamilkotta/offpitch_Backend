db40ac0b8b21175cbf864de3354c92237f6ed5e8
shamilkotta <shamilkotta99@gmail.com>
2023-07-05 21:56:07 +0530

    fix: CORS error

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Authorization, Accept"
